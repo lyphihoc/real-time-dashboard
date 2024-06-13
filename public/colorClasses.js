0e3496319373b43356e4125de3db7a83fd71b47f
Ly Hoc <hoc.ly@creqavn.net>
2024-06-13 10:32:20 +0700

    disable mongodb and add status color

---
+++ public/colorClasses.js
@@ -0,0 +1,12 @@
+const colorClasses = {
+    'red': 'color-red',
+    'yellow': 'color-yellow',
+    'gray': 'color-gray',
+    'orange': 'color-orange',
+    'blue': 'color-blue',
+    'pink': 'color-pink',
+    'black': 'color-black',
+    'green': 'color-green',
+};
+  
+export default colorClasses;
--- server.js
+++ server.js
@@ -1,39 +1,43 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const path = require('path');
 
 // Set up Express
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+const port = 3000;
 
 // Set up Mongoose
-mongoose.connect('mongodb://localhost:27017/domains', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/domains', { useNewUrlParser: true, useUnifiedTopology: true });
 
-const domainSchema = new mongoose.Schema({
-    domain: String,
-    isp: String,
-    status: String
-});
+// const domainSchema = new mongoose.Schema({
+//     domain: String,
+//     isp: String,
+//     color: String,
+//     status: String
+// });
 
-const Domain = mongoose.model('Domain', domainSchema);
+// const Domain = mongoose.model('Domain', domainSchema);
 
 // Middleware to parse JSON
 app.use(express.json());
 
 // Endpoint to receive JSON data
 app.post('/update', async (req, res) => {
-    const { domain, isp, status } = req.body;
-    const updatedDomain = await Domain.findOneAndUpdate(
-        { domain },
-        { isp, status },
-        { upsert: true, new: true }
-    );
-    console.log('Emitting statusUpdate:', updatedDomain); // Log emitted data
-    io.emit('statusUpdate', updatedDomain);
-    res.status(200).send(updatedDomain);
+    const data = req.body;
+
+    // const updatedDomain = await Domain.findOneAndUpdate(
+    //     { domain },
+    //     { isp, status, color },
+    //     { upsert: true, new: true }
+    // );
+
+    console.log('Emitting statusUpdate:', data); // Log emitted data
+    io.emit('statusUpdate', data);
+    res.status(200).send(data);
 });
 
 app.get('/', (req, res) => {
@@ -44,6 +48,6 @@ app.get('/', (req, res) => {
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Start the server
-server.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
